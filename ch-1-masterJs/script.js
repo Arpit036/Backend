cc32426749d0a0d13024e978b9c2e347b5b9d8c5
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-07-08 14:20:38 +0530

    Basics of js for Backend

---
+++ ch-1-masterJs/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Arpit")
+alert("Hello")
+/*
+topics-
+1. Fundamentals of js
+2. Arrays and objects
+3. function return
+4. async js coding
+5. foreach , map,filter,find, indexof
+6. Objects
+7. length of fucntion
+
+*/
