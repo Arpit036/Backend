004de34d7b0b83f4336f509d100ad5c48f9ce706
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-07-09 12:13:33 +0530

    routes example

--- ch-4-Express.js/script.js
+++ ch-4-Express.js/script.js
@@ -5,4 +5,16 @@ app.get('/', function(req , res) {
     res.send('Hello Arpit')
 })
 
+app.get('/Hello', function(req , res) {
+    res.send('Hello    ')
+})
+
+app.get('/About', function(req , res) {
+    res.send('About    ')
+})
+
+app.get('/contact', function(req , res) {
+    res.send('contact    ')
+})
+
 app.listen(3000)
